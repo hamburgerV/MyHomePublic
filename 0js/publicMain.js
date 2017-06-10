cb0571a2293ff5d186a1d629c85697fe1d32060e
hamburgerV <34002234@qq.com>
2017-06-10 15:03:50 +0800

    tab

---
+++ 0js/publicMain.js
@@ -0,0 +1,20 @@
+function strTOjson(str){ // 转json
+	var arr = str.split('&');
+	var json = {};
+	for(var i = 0;i<arr.length; i++){
+		json[arr[i].split(':')[0]] = arr[i].split(':')[1];
+	}
+	return json;
+}
+function jsonTOstr(json){ // 转str
+	var arr = [];
+	for(var name in json){
+		arr.push(name+':'+json[name]);
+	}
+	return arr.join('&');
+}
+function todou(n){ // 补零
+	return n > 10 ? '' + n : '0' + n; 
+}
+
+
